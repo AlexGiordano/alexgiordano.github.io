27ffa080e29af681eec92b5930cda8de592550f6
Alex Giordano <alxgiordano@hotmail.com>
2018-10-02 11:53:24 +1000

    Update navbarmodalsscript.js

--- navbarmodalsscript.js
+++ navbarmodalsscript.js
@@ -11,45 +11,37 @@ const AlexModule = (function AlexModule() {
       $el.addEventListener(type, fn, false);
     }
   };
-
   function stopVideo(modal) {
     const currentIframe = modal.querySelector('.modal-content > iframe');
     currentIframe.src = currentIframe.src;
   }
-
   const getElementArray = (className) => {
     const modals = document.getElementsByClassName(className);
     return Array.from(modals);
   };
-
   function closeClickHander() {
     const parent = this.closest('.w3-modal');
     parent.style.display = 'none';
     stopVideo(parent);
   }
-
   function openClickHander() {
     const modalId = this.dataset.modalId;
     const modal = document.getElementById(modalId)
     modal.style.display = 'block';
   }
-
   const addOpenClickHandlers = () => {
     const aEls = getElementArray('open');
     aEls.forEach((el) => {
       addEventHandler(el, 'click', openClickHander);
     });
   };
-
   const addCloseClickHandlers = () => {
     const spanEls = getElementArray('close');
     spanEls.forEach((el) => {
       addEventHandler(el, 'click', closeClickHander);
     });
   };
-
   const modalEls = getElementArray('w3-modal');
-
   function closeModalClickHander(event) {
     modalEls.forEach((elArg) => {
       const el = elArg;
@@ -59,14 +51,11 @@ const AlexModule = (function AlexModule() {
       }
     });
   }
-
   addEventHandler(window, 'click', closeModalClickHander);
-
   return {
     addCloseClickHandlers,
     addOpenClickHandlers,
   };
 }());
-
 AlexModule.addCloseClickHandlers();
 AlexModule.addOpenClickHandlers();
